c048479a1d4a22e6c0e7bcee9bcdcb1432aebb4e
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2025-01-22 22:47:39 +0100

    Create Common.hpp
    Not writing that every time

---
+++ Common.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <winsock2.h>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <string>
+
+#pragma comment(lib, "ws2_32.lib") // just found out about this: this is great
+
+#define PORT 54000
+#define BUFFER_SIZE 4096
+
