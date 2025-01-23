98f50cc6c440571de680f93f12a0b07a01c5c7c4
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2025-01-23 20:29:40 +0100

    Update Common.hpp

--- Common.hpp
+++ Common.hpp
@@ -1,13 +1,26 @@
 #pragma once
 
-#include <winsock2.h>
 #include <iostream>
 #include <thread>
 #include <vector>
 #include <string>
+#include <cstring>
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
 #pragma comment(lib, "ws2_32.lib") // just found out about this: this is great
 
 #define PORT 54000
 #define BUFFER_SIZE 4096
+//#define _CRT_SECURE_NO_WARNINGS
 
+class Discarder
+{
+public:
+	template<typename T> constexpr inline void operator=(T&) const {}
+	template<typename T> constexpr inline void operator=(T&&) const {}
+private:
+};
+
+constexpr inline Discarder _;
