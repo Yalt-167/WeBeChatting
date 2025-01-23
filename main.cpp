4048803427b4fb4bfe26e2287af34a501367bb56
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2025-01-23 20:32:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
+#include "Socket.hpp"
+#include "ClientSocket.hpp"
+#include "ServerSocket.hpp"
 
 
-
-
-
-
-
-int main(int argc, char** argv)
+int main()
 {
+	std::cout << "Choose your role:\n - Server (1)\n - Client (Whatever)\n" << std::endl;
+	int role;
+	std::cin >> role;
+
+	bool isServer = role == 1;
+
+	std::string username;
+	if (!isServer)
+	{
+		std::cout << "Enter your username: " << std::endl;
+		std::cin >> username;
+	}
+
+	Socket* socket_ = (isServer ? (Socket*)new ServerSocket() : (Socket*)new ClientSocket(username));
+
+	socket_->Run();
+
+	while (true) { }
+
+
+	delete socket_;
+
 	return 0;
 }
